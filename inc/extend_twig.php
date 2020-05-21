17283fc872ea9bd7a91bef207cfd481f3c54eee2
jarnalyrkar <joachimsolstrom@gmail.com>
2020-05-21 12:23:43 +0200

    Edit settings

--- inc/extend_twig.php
+++ inc/extend_twig.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'add_breadcrumb.php';
 require_once 'phone_number_filter.php';
+require_once 'wpml_custom_switcher.php';
 
 /*
  *  I denne funksjonen legger du til funksjoner og filter som du ønsker
@@ -12,7 +13,10 @@ function add_to_twig( $twig ) {
 
   // Du kan legge til funksjoner:
   $twig->addFunction(new Timber\Twig_Function('breadcrumb', 'breadcrumb'));
+  $twig->addFunction(new Timber\Twig_function('language_switcher', 'language_switcher'));
 
   return $twig;
 } add_filter( 'timber/twig', 'add_to_twig' );
+
+
 
