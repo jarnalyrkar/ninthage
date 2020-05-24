6d8cdeb0e298eee76328c63cc42a1c47f9523d6c
jarnalyrkar <joachimsolstrom@gmail.com>
2020-05-24 11:35:21 +0200

    Add start of faction list

--- page.php
+++ page.php
@@ -3,4 +3,9 @@ $context = Timber::context();
 $context['post'] = new Timber\Post();
 $context['languages'] = pll_the_languages(array('raw' => 1));
 
+$context['factions'] = get_posts(array(
+  'post_type' =>'faction',
+  'numberposts' => -1,
+));
+
 Timber::render('pages/page.twig', $context);
