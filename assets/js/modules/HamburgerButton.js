ba3d19ee043c55bccea1d9f5c2461a9140e339e9
jarnalyrkar <joachimsolstrom@gmail.com>
2020-05-25 11:38:39 +0200

    Add footer with navigation and credits

--- assets/js/modules/HamburgerButton.js
+++ assets/js/modules/HamburgerButton.js
@@ -10,7 +10,19 @@ class HamburgerButton {
     this.hamburger.addEventListener('click', (ev) => {
       this.nav.classList.toggle('headerMenu--open');
       this.hamburger.classList.toggle('hamburger--toggled');
+      document.body.classList.toggle('disableScroll');
+      this.closeOpenMenus();
     });
+  }
+  closeOpenMenus() {
+    this.menuItems = document.querySelectorAll('.headerMenu__list .headerMenu__item');
+
+    this.menuItems.forEach(item => {
+      if (item.classList.contains('menu-item-has-children--open')) {
+        item.classList.remove('menu-item-has-children--open');
+        item.querySelector('.headerMenu__submenu--open').classList.remove('headerMenu__submenu--open');
+      }
+    })
   }
 }
 
--- inc/add_twig_nav.php
+++ inc/add_twig_nav.php
@@ -2,12 +2,14 @@
 /* Registrer menyene som vanlig: */
 register_nav_menus(array(
   'headerMenuLocation' => 'Header Menu Location',
+  'footerMenuLocation' => 'Footer Menu Location'
   )
 );
 
-/* Legg til menyen i det globale scopet til timber  */
+/* Add menu to the global Twig scope  */
 function add_nav_to_context( $context ) {
-  // Vi instansierer et nytt Timber\Menu objekt
+  // Instantiate a new Timber\Menu object
   $context['mainNav'] = new Timber\Menu('headerMenuLocation');
+  $context['footerNav'] = new Timber\Menu('footerMenuLocation');
   return $context;
 } add_filter( 'timber/context', 'add_nav_to_context' );
