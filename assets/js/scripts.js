fc0372535c3497f0b886369ea926c0f77862cee3
jarnalyrkar <joachimsolstrom@gmail.com>
2020-05-27 22:05:40 +0200

    Add recent forum posts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,7 @@ import NavSubmenus from './modules/NavSubmenus';
 import Lightbox from './modules/Lightbox';
 import LanguageMenu from './modules/LanguageMenu';
 import Accordion from './modules/Accordion';
+import GetForumPosts from './modules/GetForumPosts';
 
 // import bootstrap from '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 // import bootstrapSlim from '../../node_modules/jquery/dist/jquery.slim.min';
@@ -15,3 +16,4 @@ const navSubmenus = new NavSubmenus();
 const lightbox = new Lightbox();
 const languageMenu = new LanguageMenu();
 const accordion = new Accordion();
+const getForumPosts = new GetForumPosts();
